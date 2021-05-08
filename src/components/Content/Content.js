0eae8ca3985fdb51fdbfe214042585c180d0880c
Juliana Meneguete <jumeneguete@gmail.com>
2021-05-08 14:32:06 -0300

    Separando as arrays de opções

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -3,90 +3,15 @@ import Footer from "../Footer";
 import Dessert from "./Dessert"
 import Drinks from "./Drinks"
 import Meals from "./Meals"
+import { dessertOptions, drinkOptions, mealOptions } from "./OptionsArrays";
 
 export default function Content() {
 
-    const [meal, setMeal] = React.useState(
-        [
-            {
-                image: "imagens/feijoada.jpg",
-                name: "Feijoada",
-                description: "Acompanha arroz, farofa, couve e laranja",
-                price: "32,90",
-                selected: false,
-                amount: 0
-            }, {
-                image: "imagens/lasanha.jpg",
-                name: "Lasanha a bolonhesa",
-                description: "Acompanha salada verde",
-                price: "39,90",
-                selected: false,
-                amount: 0
-            }, {
-                image: "imagens/frango.jpg",
-                name: "Frango com quiabo",
-                description: "Acompanha polenta cremosa e farofa",
-                price: "32,90",
-                selected: false,
-                amount: 0
-            }
-        ]
-    );
+    const [meal, setMeal] = React.useState(mealOptions);
 
-    const [drink, setDrink] = React.useState(
-        [
-            {
-                image: "imagens/coca.jpg",
-                name: "Coca-cola",
-                description: "Lata 350mL",
-                price: "5,90",
-                selected: false,
-                amount: 0
-            }, {
-                image: "imagens/mate.jpg",
-                name: "Mate Leão",
-                description: "Copo 300mL",
-                price: "4,90",
-                selected: false,
-                amount: 0
-            }, {
-                image: "imagens/h20.jpg",
-                name: "H2O",
-                description: "Garrafa 500mL",
-                price: "6,90",
-                selected: false,
-                amount: 0
-            }
-        ]
-    );
+    const [drink, setDrink] = React.useState(drinkOptions);
 
-
-    const [dessert, setDessert] = React.useState(
-        [
-            {
-                image: "imagens/pave.jpg",
-                name: "Pavê de Bis",
-                description: "Pote de 250mL",
-                price: "8,90",
-                selected: false,
-                amount: 0
-            }, {
-                image: "imagens/torta.jpg",
-                name: "Torta de Limão",
-                description: "Uma fatia",
-                price: "8,90",
-                selected: false,
-                amount: 0
-            }, {
-                image: "imagens/brigadeiro.jpg",
-                name: "H2BrigadeiroO",
-                description: "Uma unidade",
-                price: "2,90",
-                selected: false,
-                amount: 0
-            }
-        ]
-    );
+    const [dessert, setDessert] = React.useState(dessertOptions);
 
 
     function selectMeal(i) {
